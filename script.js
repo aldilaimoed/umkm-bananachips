86128c82b5f1a0c7567566d81b5957794b4226fe
aldilaaaa <aldilarachmaa@gmail.com>
2025-12-15 16:09:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Memberi tahu mahasiswa bahwa script berhasil di-load  
+alert('Selamat datang di Website Profil UMKM Banana Chips!');  
+
+// Menampilkan informasi variabel di console  
+const namaUMKM = 'Banana Chips';  
+console.log('Nama UMKM:', namaUMKM);  
+
+// Demonstrasi variabel dan operator  
+let produk = 3;  
+console.log('Jumlah produk saat ini:', produk);  
+produk += 2;  
+console.log('Setelah penambahan:', produk); 
+
+//
+let bananaTiramisu = 5;
+let bananaCoklat = 2;
+let bananaMatcha = 3;
+let totalProduk = bananaTiramisu + bananaCoklat + bananaMatcha;
+
+console.log(' Jumlah Varian Banana Tiramisu = ', bananaTiramisu);
+console.log('Jumlah Varian Banana Coklat = ', bananaCoklat);
+console.log('Jumlah Varian Banana Matcha = ', bananaMatcha);
+console.log('Total Semua Produk = ', totalProduk);
